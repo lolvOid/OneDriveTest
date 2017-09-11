733772c03068412c7fa119f52d50cd547a6327c7
lolvOid <linhtet1998itachi@gmail.com>
2017-09-11 15:16:34 +0630

    fixed bug #1

---
+++ OneDriveTest/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Identity.Client;
+namespace OneDriveTest
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private static string ClientId = "cbc1b653-d562-4e3c-977a-c24749234a05";
+
+        public static PublicClientApplication PublicClientApp = new PublicClientApplication(ClientId);
+    }
+}
